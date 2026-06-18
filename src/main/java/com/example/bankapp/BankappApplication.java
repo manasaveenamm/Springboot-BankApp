1e9ee9209e037b500704d5b875691eba6be92d85
Manasa <157228409+manasaveenamm@users.noreply.github.com>
2026-06-18 12:44:19 +0530

    Refactor BankappApplication for servlet support
    Updated BankappApplication to extend SpringBootServletInitializer for deployment.

--- src/main/java/com/example/bankapp/BankappApplication.java
+++ src/main/java/com/example/bankapp/BankappApplication.java
@@ -1,13 +1,21 @@
+
 package com.example.bankapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BankappApplication {
+public class BankappApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BankappApplication.class, args);
-	}
+    // This override block tells standalone Apache Tomcat how to launch the Spring Boot app context
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BankappApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(BankappApplication.class, args);
+    }
 }
